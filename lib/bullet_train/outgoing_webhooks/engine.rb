365ccfbb3ccdbeee987c9807c8aea580fa3168ed
Pete Keen <pete@petekeen.net>
2022-02-16 10:04:07 -0500

    Check for BulletTrain::Api using const_defined?

--- lib/bullet_train/outgoing_webhooks/engine.rb
+++ lib/bullet_train/outgoing_webhooks/engine.rb
@@ -27,7 +27,7 @@ module BulletTrain
       end
 
       initializer "bullet_train.outgoing_webhooks.register_api_endpoints" do |app|
-        if BulletTrain::Api
+        if Object.const_defined?('BulletTrain::Api')
           BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::EndpointsEndpoint"
           BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::DeliveriesEndpoint"
           BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::DeliveryAttemptsEndpoint"
