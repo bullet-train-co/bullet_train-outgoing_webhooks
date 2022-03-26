e53864b3b68384fcbb0de54c3ed706abd4eb0395
Pete Keen <pete@petekeen.net>
2022-03-25 21:17:02 -0400

    Remove most webhook database reads from hot path

--- app/models/concerns/webhooks/outgoing/issuing_model.rb
+++ app/models/concerns/webhooks/outgoing/issuing_model.rb
@@ -13,7 +13,14 @@ module Webhooks::Outgoing::IssuingModel
   module ClassMethods
   end
 
+  def skip_generate_webhook?(action)
+    false
+  end
+
   def generate_webhook(action)
+    # allow individual models to opt out of generating webhooks
+    return if skip_generate_webhook?(action)
+
     # we can only generate webhooks for objects that return their team.
     return unless respond_to? :team
 
@@ -25,25 +32,31 @@ module Webhooks::Outgoing::IssuingModel
     if event_type && team
 
       # Only generate an event record if an endpoint is actually listening for this event type.
-      if team.webhooks_outgoing_endpoints.listening_for_event_type_id(event_type.id).any?
-        data = "Api::V1::#{self.class.name}Serializer".constantize.new(self).serializable_hash[:data]
-        webhook = team.webhooks_outgoing_events.create(event_type_id: event_type.id, subject: self, data: data)
-        webhook.deliver
+      if team.endpoints_listening_for_event_type?(event_type)
+        # serialization can be heavy so run it as a job
+        Webhooks::Outgoing::GenerateJob.perform_later(self, action)
       end
     end
   end
 
+  def generate_webhook_perform(action)
+    event_type = Webhooks::Outgoing::EventType.find_by(id: "#{self.class.name.underscore}.#{action}")
+    data = "Api::V1::#{self.class.name}Serializer".constantize.new(self).serializable_hash[:data]
+    webhook = team.webhooks_outgoing_events.create(event_type_id: event_type.id, subject: self, data: data)
+    webhook.deliver
+  end
+
   def generate_created_webhook
-    generate_webhook("created")
+    generate_webhook(:created)
   end
 
   def generate_updated_webhook
-    generate_webhook("updated")
+    generate_webhook(:updated)
   end
 
   def generate_deleted_webhook
     return false unless respond_to?(:team)
     return false if team&.being_destroyed?
-    generate_webhook("deleted")
+    generate_webhook(:deleted)
   end
 end
