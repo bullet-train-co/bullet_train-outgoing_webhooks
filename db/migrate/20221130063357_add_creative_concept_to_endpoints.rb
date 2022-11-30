9db6dfad871ac9ebaa6c73a78b2456042f174d1e
ps-ruby <prakashsanyasi619@gmail.com>
2022-11-30 13:52:00 +0600

    standardrb --fix

--- db/migrate/20221130063357_add_creative_concept_to_endpoints.rb
+++ db/migrate/20221130063357_add_creative_concept_to_endpoints.rb
@@ -4,7 +4,7 @@ class AddCreativeConceptToEndpoints < ActiveRecord::Migration[7.0]
       :webhooks_outgoing_endpoints,
       :scaffolding_absolutely_abstract_creative_concept,
       foreign_key: true,
-      index: { name: 'index_endpoints_on_abstract_creative_concept_id' }
+      index: {name: "index_endpoints_on_abstract_creative_concept_id"}
     )
   end
 end
